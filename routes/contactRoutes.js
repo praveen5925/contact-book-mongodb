f8252d10e724834a9604ce5348400ceea4a79932
praveen5925 <ironman.056881@gmail.com>
2026-02-28 18:11:17 +0530

    Updated server logic

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,20 +2,20 @@ const express = require('express')
 const router = express.Router()
 const Contact = require('../models/contact')
 
-// Add Contact
+
 router.post('/', async (req, res) => {
     const contact = new Contact(req.body)
     await contact.save()
     res.json(contact)
 })
 
-// Get All Contacts
+
 router.get('/', async (req, res) => {
     const contacts = await Contact.find()
     res.json(contacts)
 })
 
-// Search Contact
+
 router.get('/search/:name', async (req, res) => {
     const contacts = await Contact.find({
         name: { $regex: req.params.name, $options: 'i' }
@@ -23,7 +23,7 @@ router.get('/search/:name', async (req, res) => {
     res.json(contacts)
 })
 
-// Delete
+
 router.delete('/:id', async (req, res) => {
     await Contact.findByIdAndDelete(req.params.id)
     res.json({ message: "Deleted" })
